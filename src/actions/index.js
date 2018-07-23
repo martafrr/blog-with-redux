cc62cb6646fdff3eb3dbb99ef508203154b3f0c8
Marta Farre <marta.farre@zalando.de>
2018-07-23 18:11:10 +0200

    navigation after new post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=itshouldbeunique';
@@ -8,9 +9,18 @@ const API_KEY = '?key=itshouldbeunique';
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
-
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
+
+    return {
+        type: CREATE_POST,
         payload: request
     };
 }
--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger': ''}`
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    { touched ? error : '' }
+                </div>
             </div>
         );
     }
 
     onSubmit(values) {
-        console.log(values)
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -41,6 +50,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -50,19 +60,21 @@ function validate(values) {
     const errors = {};
     
     if(!values.title) {
-        erros.title = 'Enter a title.';
+        errors.title = 'Enter a title.';
     }
     if(!values.categories) {
-        erros.categories = 'Enter some categories.';
+        errors.categories = 'Enter some categories.';
     }
     if(!values.content) {
-        erros.categories = 'Enter some content.';
+        errors.content = 'Enter some content.';
     }
     
-    return erros;
+    return errors;
 }
 
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
