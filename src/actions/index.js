5e219c0a95636693ad107abcb008777323f8ac17
Marta Farre <marta.farre@zalando.de>
2018-07-25 11:38:50 +0200

    deleting a post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=itshouldbeunique';
+const API_KEY = '?key=itshouldbeuniqueee';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -36,10 +37,11 @@ export function fetchPost(id) {
 }
 
 export function deletePost(id) {
-    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
 
     return {
         type: DELETE_POST,
-        payload: request
+        payload: id
     }
 }
